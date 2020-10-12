2cba62b2d41e70fd8bf8d8698ce2905e8215993e
sohyeonjin <shj41738866@gmail.com>
2020-10-13 01:22:21 +0900

    modified videouploadpage style

--- client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Typography, Button, Form, message, Input } from 'antd';
-import Icon from '@ant-design/icons';
+import { Typography, Button, Form, Input } from 'antd';
+import {PlusOutlined} from '@ant-design/icons';
 import TextArea from 'antd/lib/input/TextArea';
 import Dropzone from 'react-dropzone'
 const { Title } = Typography;
@@ -10,47 +10,56 @@ function VideoUploadPage() {
     <div style={{ maxWidth: '700px', margin: '2rem auto' }}>
       <div style={{textAlign:'center', marginBottom:'2rem'}}>
         <Title level={2}>동영상 업로드</Title>
-          </div>
+      </div>
 
-                  <Form onSubmit style={{display:'flex',flexDirection:'column',justifyContent:'space-between',alignItems:'flex-start'}}>
-        <div>
-          <div>
-            {/*Drop zone*/}\
+      <Form onSubmit>
+      <div /*style={{display:'flex', justifyContent:'space-between'}}*/>
+
+            {/*Drop zone*/}
             <Dropzone
-            onDrop 
-            multiple
-            maxsize>
-            {({getRootProps, getInputProps})=>(
-              <div style={{width: '300px', height: '240px', border:'1px solid lightgray',
-              alignItems:'center', justifyContent:'center'}} {...getRootProps()}> 
-                <input {...getInputProps()}/>
-                <Icon type="plus" style={{fontSize:'3rem'}}/>
-              </div>
-            )}
+              onDrop 
+              multiple
+              maxsize>
+              {({getRootProps, getInputProps})=>(
+                <div style={{ width: '300px', height: '240px', border:'1px solid lightgray',display:'flex',
+                alignItems:'center', justifyContent:'center'}} {...getRootProps()}> 
+                  <input {...getInputProps()}/>
+                  <PlusOutlined  style={{fontSize:'3rem'}}/>
+                </div>
+              )}
             </Dropzone>
 
             {/*Thumbnail*/}
 
-            <img />
-          </div>
+            <div>
+              <img />
+            </div>
         </div>
-        <label>Title</label>
-        <input/>
+
         <br/>
+        <br/>
+        <label>Title</label>
+        <Input/>
+        <br/>
+      <br/>
+
         <label>Description</label>
         <TextArea/>
         <br/>
+        <br/>
 
       <select>
-        <option></option>
-            </select>
+        <option key value></option>
+      </select>
+        <br/>
         <br/>
       <select>
         <option></option>
       </select>
         <br/>
+        <br/>
 
-      <Button type='primary'>
+      <Button type='primary' size='large'>
         submit
       </Button>
       </Form>
