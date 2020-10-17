65f9ba800f8a733d010f936283423fcf6af5f2be
sohyeonjin <shj41738866@gmail.com>
2020-10-18 00:20:03 +0900

    update video collection

---
+++ server/models/Video.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const videoSchema = mongoose.Schema(
+  {
+    writer: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    title: {
+      type: String,
+      maxlength: 50,
+    },
+    description: {
+      type: String,
+    },
+    privacy: {
+      type: Number,
+    },
+    filePath: {
+      type: String,
+    },
+    category: {
+      type: String,
+    },
+    views: {
+      type: Number,
+      //view 수가 처음에는 0부터 시작
+      default: 0,
+    },
+    duration: {
+      type: String,
+    },
+    thumbnail: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+const Video = mongoose.model("Video", videoSchema);
+
+module.exports = { Video };
