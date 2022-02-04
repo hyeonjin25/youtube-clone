dfda9904237fc1108d7bf45841f0ef31a847cee7
hyeonjin25 <shj41738866@gmail.com>
2022-02-05 00:54:16 +0900

    change footer

--- client/src/components/views/Footer/Footer.js
+++ client/src/components/views/Footer/Footer.js
@@ -1,16 +1,20 @@
-import React from 'react'
-import Icon from '@ant-design/icons';
+import React from "react";
 
 function Footer() {
-    return (
-        <div style={{
-            height: '80px', display: 'flex',
-            flexDirection: 'column', alignItems: 'center',
-            justifyContent: 'center', fontSize:'1rem'
-        }}>
-           <p> Happy Coding  <Icon type="smile" /></p>
-        </div>
-    )
+  return (
+    <div
+      style={{
+        height: "80px",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        fontSize: "1rem",
+      }}
+    >
+      <p> this is hyeonjin's website for youtube clone coding</p>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
